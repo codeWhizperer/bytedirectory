4e5a5ed7bd58fb568f1bf2c5e4f6e0450d576149
Adegbite Ademola Kelvin <adegbiteademolakelvin@Adegbites-MacBook-Pro.local>
2024-09-19 18:58:09 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,37 @@ mod api;
 mod model;
 mod schema;
 
+use std::any::Any;
+
+use actix_cors::Cors;
 use actix_web::{
+    http::{
+        self,
+        header::{self, CONTENT_TYPE},
+        Method,
+    },
+    web::head,
     App, HttpServer,
 };
 
-
-
-use api::{submit, get_all_selectors,get_selector_by_name};
+use api::{get_all_selectors, get_selector_by_name, submit};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_all_selectors).service(submit).service(get_selector_by_name))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin("*")
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600),
+            )
+            .service(get_all_selectors)
+            .service(submit)
+            .service(get_selector_by_name)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-
