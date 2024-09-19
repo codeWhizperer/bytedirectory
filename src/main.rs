8c4c2940a28accd880a43ce505d1fa4a199f8358
Adegbite Ademola Kelvin <adegbiteademolakelvin@Adegbites-MacBook-Pro.local>
2024-09-19 18:59:36 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(
                 Cors::default()
-                    .allowed_origin("*")
+                    .allowed_origin("http://localhost:3000")
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
